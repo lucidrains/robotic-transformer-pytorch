16da38836af48ded72f01e2c8f1f88b8cdc7d5e2
Phil Wang <lucidrains@gmail.com>
2022-12-14 13:28:21 -0800

    obvious need

--- robotic_transformer_pytorch/robotic_transformer_pytorch.py
+++ robotic_transformer_pytorch/robotic_transformer_pytorch.py
@@ -4,6 +4,83 @@ from torch import nn, einsum
 
 from einops import pack, unpack, repeat, reduce, rearrange
 
+# attention
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        causal = False,
+        dim_head = 64,
+        dim_context = None,
+        heads = 8,
+        norm_context = False,
+        dropout = 0.1
+    ):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        self.causal = causal
+        inner_dim = dim_head * heads
+
+        dim_context = default(dim_context, dim)
+
+        self.norm = LayerNorm(dim)
+        self.context_norm = LayerNorm(dim_context) if norm_context else nn.Identity()
+
+        self.attn_dropout = nn.Dropout(dropout)
+
+        self.to_q = nn.Linear(dim, inner_dim, bias = False)
+        self.to_kv = nn.Linear(dim_context, dim_head * 2, bias = False)
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim, bias = False),
+            nn.Dropout(dropout)
+        )
+
+    def forward(
+        self,
+        x,
+        context = None,
+        mask = None,
+        attn_bias = None
+    ):
+        b = x.shape[0]
+
+        if exists(context):
+            context = self.context_norm(context)
+
+        kv_input = default(context, x)
+
+        x = self.norm(x)
+
+        q, k, v = self.to_q(x), *self.to_kv(kv_input).chunk(2, dim = -1)
+
+        q = rearrange(q, 'b n (h d) -> b h n d', h = self.heads)
+
+        q = q * self.scale
+
+        sim = einsum('b h i d, b j d -> b h i j', q, k)
+
+        if exists(attn_bias):
+            sim = sim + attn_bias
+
+        if exists(mask):
+            mask = rearrange(mask, 'b j -> b 1 1 j')
+            sim = sim.masked_fill(~mask, -torch.finfo(sim.dtype).max)
+
+        if self.causal:
+            i, j = sim.shape[-2:]
+            causal_mask = torch.ones((i, j), dtype = torch.bool, device = x.device).triu(j - i + 1)
+            sim = sim.masked_fill(causal_mask, -torch.finfo(sim.dtype).max)
+
+        attn = sim.softmax(dim = -1)
+        attn = self.attn_dropout(attn)
+
+        out = einsum('b h i j, b j d -> b h i d', attn, v)
+
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
 # token learner module
 
 class TokenLearner(nn.Module):

00ad917b62f356b3c30cac45f7d8474b983a5df1
Phil Wang <lucidrains@gmail.com>
2022-12-14 18:09:26 -0800

    need latest einops

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'robotic-transformer-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.2',
+  version = '0.0.3',
   license='MIT',
   description = 'Robotic Transformer - Pytorch',
   author = 'Phil Wang',
@@ -19,7 +19,7 @@ setup(
   ],
   install_requires=[
     'classifier-free-guidance-pytorch',
-    'einops>=0.4',
+    'einops>=0.6',
     'torch>=1.6',
   ],
   classifiers=[

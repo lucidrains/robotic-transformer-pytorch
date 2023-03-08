a5229e99e3d7178d798b199c34e9bf0f7080fd28
Phil Wang <lucidrains@gmail.com>
2023-03-08 12:23:11 -0800

    enforce latest classifier free guidance repo version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'robotic-transformer-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.14',
+  version = '0.0.15',
   license='MIT',
   description = 'Robotic Transformer - Pytorch',
   author = 'Phil Wang',
@@ -18,7 +18,7 @@ setup(
     'robotics'
   ],
   install_requires=[
-    'classifier-free-guidance-pytorch>=0.0.23',
+    'classifier-free-guidance-pytorch>=0.1.2',
     'einops>=0.6',
     'torch>=1.6',
   ],
